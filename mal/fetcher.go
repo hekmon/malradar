ee2a72aef838648c316d82ca0a0d29d8708b48f8
Hekmon <edouardhur@gmail.com>
2020-05-30 14:10:40 +0200

    do not add all animes to initial listing

--- mal/fetcher.go
+++ mal/fetcher.go
@@ -8,13 +8,13 @@ import (
 )
 
 const (
-	freq                          = 24 * time.Hour
-	estimatedMaxAnimesInOneSeason = 200
+	fetchFreq          = 24 * time.Hour
+	animeStatusOnGoing = "TBA"
 )
 
 func (c *Controller) fetcher() {
 	// create the ticker
-	ticker := time.NewTicker(freq)
+	ticker := time.NewTicker(fetchFreq)
 	defer ticker.Stop()
 	// start the first batch
 	c.batch()
@@ -54,8 +54,8 @@ func (c *Controller) buildInitialList() (err error) {
 	var (
 		seasonList   *jikan.Season
 		animeDetails *jikan.Anime
+		added        int
 	)
-	c.watchList = make(map[int]*jikan.Anime, c.nbSeasons*estimatedMaxAnimesInOneSeason)
 	for i := 0; i < c.nbSeasons; i++ {
 		c.log.Debugf("[MAL] [Fetcher] building initial list: season %d/%d (%s %d)",
 			i+1, c.nbSeasons, season, year)
@@ -66,7 +66,11 @@ func (c *Controller) buildInitialList() (err error) {
 				i+1, season, year, err)
 			return
 		}
+		if c.watchList == nil {
+			c.watchList = make(map[int]*jikan.Anime, c.nbSeasons*len(seasonList.Anime))
+		}
 		// get details
+		added = 0
 		for index, anime := range seasonList.Anime {
 			c.rateLimiter()
 			if animeDetails, err = jikan.GetAnime(anime.MalID); err != nil {
@@ -74,12 +78,19 @@ func (c *Controller) buildInitialList() (err error) {
 					i+1, season, year, anime.MalID, err)
 				return
 			}
-			c.log.Debugf("[MAL] [Fetcher] building initial list: season %d/%d (%s %d): anime %d/%d: '%s' (ID %d) details fetched",
-				i+1, c.nbSeasons, season, year, index, len(seasonList.Anime), animeDetails.TitleEnglish, animeDetails.MalID)
-			c.watchList[anime.MalID] = animeDetails
+			switch animeDetails.Status {
+			case animeStatusOnGoing:
+				c.log.Debugf("[MAL] [Fetcher] building initial list: season %d/%d (%s %d): anime %d/%d: '%s' (ID %d) is %s: adding it to the list",
+					i+1, c.nbSeasons, season, year, index, len(seasonList.Anime), animeDetails.TitleEnglish, animeDetails.MalID, animeDetails.Status)
+				c.watchList[anime.MalID] = animeDetails
+				added++
+			default:
+				c.log.Debugf("[MAL] [Fetcher] building initial list: season %d/%d (%s %d): anime %d/%d: '%s' (ID %d) is %s: skipping",
+					i+1, c.nbSeasons, season, year, index, len(seasonList.Anime), animeDetails.TitleEnglish, animeDetails.MalID, animeDetails.Status)
+			}
 		}
-		c.log.Infof("[MAL] [Fetcher] building initial list: season %d/%d (%s %d): recovered %d animes",
-			i+1, c.nbSeasons, season, year, len(seasonList.Anime))
+		c.log.Infof("[MAL] [Fetcher] building initial list: season %d/%d (%s %d): added %d/%d animes",
+			i+1, c.nbSeasons, season, year, added, len(seasonList.Anime))
 		// prepare for next run
 		season, year = getPreviousSeason(season, year)
 	}
