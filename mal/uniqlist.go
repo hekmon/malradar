23840080e94b116a46330a98335a2d2cf2808ad9
Hekmon <edouardhur@gmail.com>
2020-05-31 19:09:37 +0200

    fix uniqlist

--- mal/uniqlist.go
+++ mal/uniqlist.go
@@ -11,11 +11,11 @@ func (ul uniqList) add(item string) {
 	ul[item] = struct{}{}
 }
 
-func (ul *uniqList) MarshalJSON() (data []byte, err error) {
+func (ul uniqList) MarshalJSON() (data []byte, err error) {
 	// create the flat list
-	flat := make([]string, len(*ul))
+	flat := make([]string, len(ul))
 	index := 0
-	for item := range *ul {
+	for item := range ul {
 		flat[index] = item
 		index++
 	}
@@ -23,14 +23,14 @@ func (ul *uniqList) MarshalJSON() (data []byte, err error) {
 	return json.Marshal(flat)
 }
 
-func (ul *uniqList) UnmarshalJSON(data []byte) (err error) {
+func (ul uniqList) UnmarshalJSON(data []byte) (err error) {
 	var flat []string
 	if err := json.Unmarshal(data, &flat); err != nil {
 		return fmt.Errorf("cannot unmarshal data wihtin the temporary flat list: %w", err)
 	}
-	*ul = make(uniqList, len(flat))
+	ul = make(uniqList, len(flat))
 	for _, item := range flat {
-		(*ul)[item] = struct{}{}
+		ul[item] = struct{}{}
 	}
 	return
 }
